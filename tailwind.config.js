de124a71ed3e401f70da23ea807abbc07b2d5e17
asasasxzc-sudo <asasasxzc@gmail.com>
2026-04-13 15:45:32 +0800

    Rename tailwind.config.ts to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -11,4 +10,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
