346ba960be4ccb7e17840243a8379a0d5cdf540c
asasasxzc-sudo <asasasxzc@gmail.com>
2026-04-13 14:02:54 +0800

    Refactor RootLayout function parameter formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className="m-0 p-0"> {/* 确保 body 没有任何奇怪的背景色覆盖 */}
+      <body className="m-0 p-0"> {/* 确保 body 没有背景色 */}
         {children}
       </body>
     </html>
