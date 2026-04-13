74223e049aaec70809f8bd1c9b376a113112ebb9
asasasxzc-sudo <asasasxzc@gmail.com>
2026-04-13 15:48:45 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/*.{js,ts,jsx,tsx,mdx}", // 强制扫描 app 根目录
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "page.tsx", // 暴力指定主页面
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
