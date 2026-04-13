ad0f1874522b89690d54868a4003ed2a08508d63
asasasxzc-sudo <asasasxzc@gmail.com>
2026-04-13 15:14:04 +0800

    Remove comment and add new content path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
-// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 必须有这一行，否则样式无法编译
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
