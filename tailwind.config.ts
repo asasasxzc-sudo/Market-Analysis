f5ec03fdc24c8e22e2deb1b5dcf54cc0af8cbbbf
asasasxzc-sudo <asasasxzc@gmail.com>
2026-04-13 14:31:06 +0800

    Fix content paths in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 检查这一行！
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 必须有这一行，否则样式无法编译
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
